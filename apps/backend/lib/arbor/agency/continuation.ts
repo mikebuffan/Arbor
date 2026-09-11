582eab03077a54599dcc1293fcf0de8cd0d54085
Aurixus <mikebuffan@gmail.com>
2026-09-11 09:10:27 -0700

    Recognize repeated follow-through corrections as agency continuation

--- apps/backend/lib/arbor/agency/continuation.ts
+++ apps/backend/lib/arbor/agency/continuation.ts
@@ -4,7 +4,7 @@ const CONTINUATION =
   /^(?:go|okay|ok|continue|keep going|do it|finish it|yes|yep|yeah|please do|carry on|go ahead)[.!?\s]*$/i;
 
 const PROCESS_CORRECTION =
-  /\b(?:don'?t wait(?: for me)?|do not wait(?: for me)?|don'?t ask me|do not ask me|keep working|keep going|you stopped|you didn'?t go|you did(?:n'?t| not) do it|you(?:'re| are) not done|not done yet|finish what you can|do what you can|carry it through|follow through|stop handing it back|don'?t hand it back|why did you stop|you tell me to do my thing|you don'?t do your thing)\b/i;
+  /\b(?:don'?t wait(?: for me)?|do not wait(?: for me)?|don'?t ask me|do not ask me|keep working|keep going|you stopped|you didn'?t go|you did(?:n'?t| not) do it|you did it again|you just did it again|you(?:'re| are) not done|not done yet|finish what you can|do what you can|carry it through|follow through|stop handing it back|don'?t hand it back|why did you stop|don'?t make me babysit|do not make me babysit|i don'?t want to tell you to go|i do not want to tell you to go|you tell me to do my thing|you don'?t do your thing)\b/i;
 
 const STATUS_CHECK =
   /^(?:what now|now what|did you finish|are you done|is it done|what(?:'s| is) going on|where are we|what are we doing|what were we doing|then what)[.!?\s]*$/i;
